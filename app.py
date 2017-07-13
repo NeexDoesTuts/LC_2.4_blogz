cff4e9e16c1d64f318dfe7a07d157af4dca6e256
neex <neex-io@users.noreply.github.com>
2017-07-12 20:22:21 -0700

    Add Post model and create development database

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, redirect, request, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-app.config["SQLALCHEMY_DATABASE_URL"] = "mysql+pymysql://build-a-blog:buildablog@localhost:3306/build-a-blog"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://build-a-blog:buildablog@localhost:3306/build-a-blog"
 app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
+
+class Post(db.Model):
+    __tablename__ = "posts"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    body = db.Column(db.Text)
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
 
 @app.route("/")
 def index():
@@ -14,7 +25,7 @@ def index():
 
 @app.route("/blog")
 def blog():
-    return "hello there"
+    return "you are on blog route"
 
 if __name__ == "__main__":
     app.run()
